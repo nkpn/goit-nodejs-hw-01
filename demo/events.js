9c9ea9703ad3fcb274dfa4ff0b2c9ae84136f340
nkpn <illinmikita@gmail.com>
2021-09-28 18:49:28 +0300

    Made this text English

--- demo/events.js
+++ demo/events.js
@@ -2,16 +2,6 @@ const EventEmitter = require('events');
 
 const emmitter = new EventEmitter();
 
-//* не работает
-// emmitter.on(event: 'anything', listener: data => {
-//     console.log('ON: anything', data)
-// })
-
-// emmitter.emit(event:'anything', args{ a:1})
-
-// setTimeout(() => {
-//     emmitter.emit('anything', {c:3})
-// }, 1500)
 
 class Dispatcher extends EventEmitter {
   subscribe(eventName, callBack) {
